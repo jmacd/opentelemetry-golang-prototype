abf6ecfcb8be6f758ff549528dafc610e177c1f1
jmacd <jmacd@lightstep.com>
2019-06-04 12:12:50 -0700

    Rename

--- api/log/log.go
+++ api/log/log.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/lightstep/opentelemetry-golang-prototype/api/core"
 	"github.com/lightstep/opentelemetry-golang-prototype/api/scope"
+	"github.com/lightstep/opentelemetry-golang-prototype/exporter/observer"
 )
 
 type (
@@ -31,6 +32,21 @@ func Logf(ctx context.Context, fmt string, args ...interface{}) {
 }
 
 func (l Logger) Log(ctx context.Context, msg string, fields ...core.KeyValue) {
+	observer.Record(observer.Event{
+		Type:       observer.LOG_EVENT,
+		Scope:      l.Scope.ScopeID(),
+		String:     msg,
+		Attributes: fields,
+		Context:    ctx,
+	})
 }
+
 func (l Logger) Logf(ctx context.Context, fmt string, args ...interface{}) {
+	observer.Record(observer.Event{
+		Type:      observer.LOGF_EVENT,
+		Scope:     l.Scope,
+		String:    fmt,
+		Arguments: args,
+		Context:   ctx,
+	})
 }
